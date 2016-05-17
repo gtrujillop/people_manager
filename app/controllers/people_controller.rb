98154c4adeb95b801f4b94412c3f602572001356
gtrujillop <gaston.trujillo.java@gmail.com>
2016-05-17 01:03:45 -0500

    Rename persons_controller as people_controller.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,32 +1,31 @@
-class PersonsController < ApplicationController
+class PeopleController < ApplicationController
 
-  # GET /persons
-  # GET /persons.json
+  # GET /people
+  # GET /people.json
   def index
-    @persons = Person.all
-
-    render json: @person, each_serializer: PersonSerializer, root: false
+    @people = Person.all
+    render json: @person, root: false
   end
 
-  # GET /persons/1
-  # GET /persons/1.json
+  # GET /people/1
+  # GET /people/1.json
   def show
     render json: @person
   end
 
-  # POST /persons
-  # POST /persons.json
+  # POST /people
+  # POST /people.json
   def create
     @person = Person.new(person_params)
     if @person.save
-      render json: @person, serializer: PersonSerializer, root: false,  status: :created, location: @person
+      render json: @person, root: false,  status: :created, location: @person
     else
       render json: @person.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /persons/1
-  # PATCH/PUT /persons/1.json
+  # PATCH/PUT /people/1
+  # PATCH/PUT /people/1.json
   def update
     @person = Person.find(params[:id])
 
@@ -37,8 +36,8 @@ class PersonsController < ApplicationController
     end
   end
 
-  # DELETE /persons/1
-  # DELETE /persons/1.json
+  # DELETE /people/1
+  # DELETE /people/1.json
   def destroy
     @person.destroy
 
