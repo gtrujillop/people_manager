0805623e291cd71047620aaf436736756a7c38ef
gtrujillop <gaston.trujillo.java@gmail.com>
2016-05-18 17:55:58 -0500

    Configure redis and resque for heroku production env.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,9 @@
-Resque.redis = 'localhost:6379'
-Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
+
+# Establish a connection between Resque and Redis
+if Rails.env == "production"
+  uri = URI.parse ENV["REDISCLOUD_URL"]
+  Resque.redis = Redis.new host:uri.host, port:uri.port, password:uri.password
+else
+  Resque.redis = 'localhost:6379'
+  Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
+end
